2cbc9ecae5341f547bee5551d751ae344065ee15
Kristian Ekle <kristian.ekle@gmail.com>
2016-10-12 21:00:06 +0200

    Fix imports

--- ldapdb/backends/ldap/base.py
+++ ldapdb/backends/ldap/base.py
@@ -5,12 +5,18 @@
 from __future__ import unicode_literals
 
 import ldap
+import django
 
-from django.db.backends.base.features import BaseDatabaseFeatures
-from django.db.backends.base.operations import BaseDatabaseOperations
-from django.db.backends.base.base import BaseDatabaseWrapper
-from django.db.backends.base.creation import BaseDatabaseCreation
-from django.db.backends.base.validation import BaseDatabaseValidation
+if django.VERSION < (1, 8):
+    from django.db.backends import (BaseDatabaseFeatures, BaseDatabaseOperations,
+                                    BaseDatabaseWrapper)
+    from django.db.backends.creation import BaseDatabaseCreation
+else:
+    from django.db.backends.base.features import BaseDatabaseFeatures
+    from django.db.backends.base.operations import BaseDatabaseOperations
+    from django.db.backends.base.base import BaseDatabaseWrapper
+    from django.db.backends.base.creation import BaseDatabaseCreation
+    from django.db.backends.base.validation import BaseDatabaseValidation
 
 
 class DatabaseCreation(BaseDatabaseCreation):
